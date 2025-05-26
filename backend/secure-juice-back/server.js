f1debc5b4ec03a3e20f6fc8c34fa55ccafe77e51
AeonPSR <floriandevigne1@hotmail.fr>
2025-05-26 10:46:25 -0400

    Initial config

---
+++ backend/secure-juice-back/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const session = require('express-session');
+const csrf = require('csurf');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'dev-secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true, sameSite: 'strict' }
+}));
+app.use(csrf());
+
+app.get('/', (req, res) => {
+  res.send('Secure Juice API is running.');
+});
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
